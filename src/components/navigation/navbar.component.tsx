a1fd5fdb43efbde2ac70b5317e92f429ae048f2a
Yerti <chessboy5@icloud.com>
2022-08-27 15:42:19 -0500

    :art: Update visuals of export button for project

--- src/components/navigation/navbar.component.tsx
+++ src/components/navigation/navbar.component.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
                 <div className="grid flex-1 md:order-2 justify-items-end">
                     <button
                         type="button"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        className="text-white font-special text-[#bcbcbc] px-4 rounded-md border-2 border-red-highlight p-2 hover:text-white text-left hover:bg-red-highlight"
                     >
                         Export to Musescore
                     </button>
